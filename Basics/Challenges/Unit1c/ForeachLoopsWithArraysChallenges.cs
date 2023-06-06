5fba34aa98ac5db524ccf6408e03bcfa3cc09b0e
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-06-06 00:11:02 -0600

    Create ForeachLoopsWithArraysChallenges.cs

---
+++ Basics/Challenges/Unit1c/ForeachLoopsWithArraysChallenges.cs
@@ -0,0 +1,11 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+	string[] movies = {"Corpse Bride", "Sweeney Todd", "Frankenweenie", "Edward Scissorhands"};
+		foreach(var movie in movies) {
+			Console.WriteLine("I love the movie " + movie + ", don't you?");		}
+	}
+}
